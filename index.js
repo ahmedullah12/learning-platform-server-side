8b7592d5b2138864170c484541aa860522f1cc44
ahmedullah12 <gulamjoy321@gmail.com>
2022-10-27 15:12:51 +0600

    all course data sent in api

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+app.use(cors());
+
 const port = process.env.PORT || 5000;
+
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
     res.send('Api running')
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
 })
+
+
+
 app.listen(port, () => console.log('server running on ', port))
